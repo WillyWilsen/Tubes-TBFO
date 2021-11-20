a075e1d850e3ef9ebb76b4d3a2ddec32be337eb6
ghaziankamil <16520465@std.stei.itb.ac.id>
2021-11-20 11:51:59 +0700

    test main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 #Program Utama Compiler#
 from converter import CFGfromTXT, CFGtoCNF, displayGrammar, turntoCNF
 
+
+
+
 CFG = CFGfromTXT("grammar.txt")
-
 #displayGrammar(CFG)
-
 CNF = CFGtoCNF(CFG)
 
 displayGrammar(CNF)
