a7cd959903a99488e23336733db3c8163e70aa3f
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2021-11-21 19:32:29 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from CYK import CYK
 import sys
 import re
 
-mkey = {"if" : "a", "elif" : "b", "else" : "c", "for" : "d", "in" : "e", "while" : "f", "continue" : "g", "pass" : "h", "break" : "i", "class" : "j", "def" : "k", "return" : "l", "as" : "m", "import" : "n", "from" : "o", "raise" : "p", "and" : "q", "or" : "r", "not" : "s", "is" : "t", "True" : "u", "False" : "v", "None" : "w", "with" : "A"}
+mkey = {"if": "a", "elif": "b", "else": "c", "for": "d", "in": "e", "while": "f", "continue": "g", "pass": "h", "break": "i", "class": "j", "def": "k", "return": "l",
+        "as": "m", "import": "n", "from": "o", "raise": "p", "and": "q", "or": "r", "not": "s", "is": "t", "True": "u", "False": "v", "None": "w", "with": "A"}
+
 
 def processInput(inp):
     global key
@@ -13,7 +15,7 @@ def processInput(inp):
     newInp = ""
 
     while inp:
-        x = re.search("[A-Za-z_][A-Za-z0-9_]*",inp)
+        x = re.search("[A-Za-z_][A-Za-z0-9_]*", inp)
         if x != None:
             newInp += inp[:x.span()[0]]
             if x.group() not in mkey:
@@ -24,12 +26,12 @@ def processInput(inp):
         else:
             newInp += inp
             inp = ""
-    
-    newInp = re.sub("[0-9]+[A-Za-z_]+","R",newInp)
-    newInp = re.sub("[0-9]+","y",newInp)
-    newInp = re.sub("#.*","",newInp)
-    mltstr = re.findall(r'([\'\"])\1\1(.*?)\1{3}',newInp,re.DOTALL)
-    
+
+    newInp = re.sub("[0-9]+[A-Za-z_]+", "R", newInp)
+    newInp = re.sub("[0-9]+", "y", newInp)
+    newInp = re.sub("#.*", "", newInp)
+    mltstr = re.findall(r'([\'\"])\1\1(.*?)\1{3}', newInp, re.DOTALL)
+
     for i in range(len(mltstr)):
         multi = mltstr[i][0]*3 + mltstr[i][1] + mltstr[i][0]*3
         newInp = newInp.replace(multi, "z\n" * mltstr[i][1].count("\n"))
@@ -39,14 +41,16 @@ def processInput(inp):
         one = str[i][0] + str[i][1] + str[i][0]
         newInp = newInp.replace(one, "z")
 
-    newInp = newInp.replace(" ","")
-    newInp = re.sub("[xyz]{1}:[xyz]{1},","",newInp)
+    newInp = newInp.replace(" ", "")
+    newInp = re.sub("[xyz]{1}:[xyz]{1},", "", newInp)
     return (newInp + '\n')
+
 
 def fileReader(path):
     with open(path, "r") as f:
         content = f.read()
     return content
+
 
 def nameError(inp):
     newInp = ""
@@ -54,11 +58,12 @@ def nameError(inp):
         x = re.search("[0-9]+[A-Za-z_]+", inp)
         if x != None:
             newInp += inp[:x.span()[0]] + x.group()
-            inp  = inp[x.span()[1]:]
+            inp = inp[x.span()[1]:]
         else:
             newInp += inp
             inp = ""
     return (newInp)
+
 
 def startCompiler():
     print("[]================ SELAMAT DATANG DI PYTHON COMPILER=================[]")
@@ -82,47 +87,43 @@ if __name__ == "__main__":
 
     startCompiler()
 
-    #input grammar
+    # input grammar
     CFG = CFGfromTXT("grammar.txt")
     CNF = CFGtoCNF(CFG)
 
-
-    #input file
+    # input file
     if (len(sys.argv) < 2):
         filePathInput = "inputAcc.py"
     else:
         filePathInput = sys.argv[1]
-    
+
     try:
         inp = fileReader(filePathInput)
     except Exception as e:
-        print("Error: "+ str(e))
+        print("Error: " + str(e))
         print("Terminating program...\n")
         exit(0)
-    
+
     inpHighlighted = nameError(inp)
     source = inp
 
     inp = processInput(inp)
-    
 
     print("Compiling " + str(filePathInput) + "...\n")
     print("Waiting for your verdict...\n")
 
-    print("=================== Source Code ================")
+    print("=========================== Source Code ===============================")
     for i, line in enumerate(inpHighlighted.split("\n")):
         idx = f" {i + 1} | " if len(str(i+1)) == 1 else\
-              f" {i + 1} | " if len(str(i+1)) == 2 else\
-              f"{i+1} | "
+            f" {i + 1} | " if len(str(i+1)) == 2 else\
+            f"{i+1} | "
         print(idx + line)
 
-    print("\n============================== Verdict =======================\n")
+    print("\n============================== Verdict =================================\n")
     if (len(inp) == 0):
         print("Accepted")
-        print("\n============================================================")
+        print("\n====================================================================")
         exit(0)
-    
+
     CYK(inp, CNF, source)
-    print("\n================================================================")
-
-
+    print("\n========================================================================")
