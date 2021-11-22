392fa4bb70ea545e3aad89d2164d160089c771a5
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-23 02:24:14 +0700

    Update test1.py

--- src/testcase/test1.py
+++ src/testcase/test1.py
@@ -1,14 +1,24 @@
 from math import __builtins__
 
-def f(x):
+def coba(x):
+    s = 1
+    T = [0 for i in range(x)]
     if (x == 10):
         print(10)
     elif (x == 3):
         print(3)
     else:
         x = 7
-        T = [0 for i in range(x)]
         T[3] = x
 
     for i in range(x):
         print(i)
+        if (i == 6):
+            s = "5"
+            pass
+    return s
+
+y = 5
+z = coba(y)
+if (type(z) == str):
+    raise TypeError
