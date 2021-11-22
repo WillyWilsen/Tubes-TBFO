f90a2cbad912f8307a90ae1250d2c3786a6a21eb
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-23 02:48:03 +0700

    Create test3.py

---
+++ src/testcase/test3.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def matriks(x):
+    if (type(x) != int):
+        raise TimeoutError
+    else:
+        i = 2
+    x = np.add(x,x)
+    y = np.transpose(x)
+    for i in range(5):
+        print("x"+x)
+        if (i == 3):
+            x = 3
+            pass
+        else:
+            continue
+        print(y)
