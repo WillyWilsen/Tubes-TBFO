d42bcdeb8e0518f8f60ca3d3554e90b1940ed4fc
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-23 01:35:53 +0700

    Create test1.py

---
+++ src/testcase/test1.py
@@ -0,0 +1,14 @@
+from math import __builtins__
+
+def f(x):
+    if (x == 10):
+        print(10)
+    elif (x == 3):
+        print(3)
+    else:
+        x = 7
+        T = [0 for i in range(x)]
+        T[3] = x
+
+    for i in range(x):
+        print(i)
