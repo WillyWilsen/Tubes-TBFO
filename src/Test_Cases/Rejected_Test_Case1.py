9be9c0707344fd2d622c662fbc8b469477ceea43
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-11-25 00:25:14 +0700

    Add demo testcase

---
+++ src/Test_Cases/Rejected_Test_Case1.py
@@ -0,0 +1,30 @@
+import package
+from package1 import function
+from package1 import function1, function2
+
+# This is a sample single-line comment
+
+def function4(param1,param2):
+    for i in range(param1)
+        print(param2)
+        if i == param1:
+            break
+        else:
+            continue
+
+# This
+# Is
+# A
+# Multiline
+# Comment
+
+x = "ABCDEFGJIJ" + 1None
+
+if not False: 
+    function4(3,x)
+elif True: 
+    print("True")
+else: 
+    print("Not True")
+
+# End of program
---
+++ src/Test_Cases/Rejected_Test_Case2.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from copy import copy
+
+class Pandaren:
+    def __init__(self, csvfile):
+        self.df = pd.read_csv(csvfile)
+    
+    def show(self):
+        print(self.df)
+    
+    def drop(self,col):
+        self.df.drop(columns=col)
+    
+    def getDF(self):
+        return self.df
+    
+    def getDuplicateDF(self):
+        return copy(self.df)
+
+return
+
+instance = Pandaren("namafile.csv","paraminiharusnyagadatapigapapakarenabukansyntaxerror")
+
+instance2 = instance.getDF()
+
+instance3 = instance.getDuplicateDF()
+
+illegal = ["col1" "col2" "col3"]
+for illegal_col in illegal:
+    instance.drop(illegal_col)
+
+if instance2.columns = instance.columns:
+    print("See, i told you it would refer to the same df")
+
+if instance3.columns != instance.columns:
+    print("See, the one using copy() didnt get changed, the columns still all there")
+
